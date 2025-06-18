fda7189063390c812fd54bb8262d201a399873b8
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-06-18 16:14:31 -0500

    init finished

--- app/src/main/java/com/example/kittmonitor/MainActivity.kt
+++ app/src/main/java/com/example/kittmonitor/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             KITTMonitorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Joe",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
