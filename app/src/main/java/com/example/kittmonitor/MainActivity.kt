00be248e3c6c58b6652ab8a045c038132753142d
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-06-19 01:02:03 -0500

    polish

--- app/src/main/java/com/example/kittmonitor/MainActivity.kt
+++ app/src/main/java/com/example/kittmonitor/MainActivity.kt
@@ -5,33 +5,35 @@ package com.example.kittmonitor
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.bluetooth.*
-import android.bluetooth.le.*
+import android.app.AlertDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.AnnotatedString
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.io.File
-import android.widget.Toast
-import android.app.AlertDialog
-import androidx.core.content.FileProvider
-import android.net.Uri
-import androidx.annotation.RequiresApi
-
-import com.example.kittmonitor.DescriptorWriteQueue
 
 class MainActivity : ComponentActivity() {
     private lateinit var bluetoothAdapter: BluetoothAdapter
@@ -45,6 +47,7 @@ class MainActivity : ComponentActivity() {
 
     private val descriptorQueue = DescriptorWriteQueue()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,6 +72,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @RequiresPermission(allOf = [Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT])
     private fun beginConnectionFlow() {
         statusTextState.value = "Searching..."
         isConnectedState.value = false
@@ -153,9 +157,7 @@ class MainActivity : ComponentActivity() {
                             logMessages = logMessages,
                             isConnectedState = isConnectedState,
                             statusTextState = statusTextState,
-                            attemptReconnect = {
-                                attemptFullConnection(onStatusChange, onDisconnected)
-                            },
+                            attemptReconnect = { attemptFullConnection(onStatusChange, onDisconnected) },
                             onStatusChange = onStatusChange,
                             onDisconnected = onDisconnected
                         )
